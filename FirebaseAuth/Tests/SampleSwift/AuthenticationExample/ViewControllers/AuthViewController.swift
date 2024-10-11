49dfae14d2f87d3f99b9788978071e94e4986059
Nick Cooke <nickcooke@google.com>
2024-10-11 17:03:09 -0400

    Remove unneeded extension and finish renaming

--- FirebaseAuth/Tests/SampleSwift/AuthenticationExample/ViewControllers/AuthViewController.swift
+++ FirebaseAuth/Tests/SampleSwift/AuthenticationExample/ViewControllers/AuthViewController.swift
@@ -340,7 +340,7 @@ class AuthViewController: UIViewController, DataSourceProviderDelegate {
   }
 
   private func performDemoEmailPasswordLoginFlow() {
-    let loginView = LoginViewSwiftUI()
+    let loginView = LoginView()
     let hostingController = UIHostingController(rootView: loginView)
     navigationController?.pushViewController(hostingController, animated: true)
   }
